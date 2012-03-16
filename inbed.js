380062edb047c988597bf4642340dc13f39dc79f
molly <molly@ciabatta.(none)>
2012-03-16 19:39:19 -0400

    first commit

---
+++ inbed.js
@@ -0,0 +1,63 @@
+var chars = {
+	"!":"",
+	".":"",
+	"?":"",
+	";":"",
+	" ":""
+}
+
+var changeListener = function() {
+	document.body.addEventListener('DOMNodeInserted', function(event) {
+        findNewBeds(event.target);
+    });
+}
+
+var findNewBeds = function(node) {
+	try {
+    	var newBed = node.getElementsByClassName("messageBody");
+    } catch(TypeError) {}
+    if (newBed) putInBed(newBed);
+}
+
+var initialChange = function() {
+	changeListener();
+	var nodes = document.getElementsByClassName("messageBody");
+	putInBed(nodes);
+}
+
+var putInBed = function(nodes) {
+	
+	for (var i=0;i<nodes.length;i++) {
+		if(nodes[i].nodeType === 1 && nodes[i].hasOwnProperty('tagName')) {
+			var replaceVal = 1;
+			var span = nodes[i];
+			var text = span.textContent;
+			if (text.substring((text.length-11), (text.length)).indexOf(" in bed") >= 0) {
+				continue;
+			}
+			var last = text.substring((text.length-1), (text.length));
+			if (last in chars) {
+				if (last === " ") {
+					replaceVal = 2;
+				}
+				if (last === "." && text.substring((text.length-3), (text.length)) === '...') {
+					replaceVal = 3;
+					last = "...";
+				}
+				span.textContent = span.textContent.substring(0, (text.length-replaceVal));
+				span.textContent = span.textContent+" in bed"+last;
+			}
+			else {
+				span.textContent += " in bed.";
+			}
+		}
+	}
+	//TODO hijack facebook's ajax response on pulldown, but ignore things that already end in "in bed".
+	// package it up into a chrome extension!
+}
+
+chrome.extension.sendRequest({checkPaused: "hello"}, function(response) {
+    if (response.maybePaused!="yes") {
+    	initialChange();
+	}
+});
