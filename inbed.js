b6bc9d22e091ef1a5c63b3ee32509f541a132879
= <=>
2012-03-17 13:45:34 -0400

    now with regex!

--- inbed.js
+++ inbed.js
@@ -1,13 +1,3 @@
-var chars = {
-	"!":"",
-	".":"",
-	"?":"",
-	";":"",
-	"\"":"",
-	"'":"",
-	" ":""
-}
-
 var changeListener = function() {
 	document.addEventListener('DOMNodeInserted', function(event) {
         findNewBeds(event.target);
@@ -35,13 +25,26 @@ var putInBed = function(nodes) {
 			var span = nodes[i];
 			var text = span.textContent;
 			var last = text.substring((text.length-1), (text.length));
-			if (last in chars) {
+			if (text.substring((text.length-8), (text.length)) === "See More") {
+				// leave it alone
+				continue;
+			}
+			if (last.match(/[\.,!;: \?\"\'()]/)) {
 				if (last === " ") {
+					// deal with trailing whitespace
 					replaceVal = 2;
+					last = text.substring((text.length-2), (text.length));
 				}
 				if (last === "." && text.substring((text.length-3), (text.length)) === '...') {
 					replaceVal = 3;
 					last = "...";
+				}
+				if (last === ")" || last === "(") {
+					if (text.substring((text.length-2), (text.length-1)) === ":") {
+						// probably an emoticon
+						replaceVal = 2;
+						last = text.substring((text.length-2), (text.length-1));
+					}
 				}
 				span.textContent = span.textContent.substring(0, (text.length-replaceVal));
 				span.textContent = span.textContent+" in bed"+last;
@@ -53,6 +56,7 @@ var putInBed = function(nodes) {
 	}
 	//TODO: handle posts that end with a url
 	//TODO: handle some other punctuation combos, emoticons, etc.
+	//TODO: handle timeline and page posts as well as news feed
 }
 
 initialChange();
